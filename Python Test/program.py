c4f3d88b10dca14a1d2dd8a56d19741d25a62e56
Joy Offere <113602216+JoyOffere@users.noreply.github.com>
2024-04-08 22:21:51 +0300

    Place holder

---
+++ Python Test/program.py
@@ -0,0 +1,7 @@
+# program.py
+
+parsecs_input = input("Input number of parsecs:")
+parsecs = int(parsecs_input)
+lightyears = 3.26156 * parsecs
+
+print(parsecs_input + " parsecs is " + str(lightyears) + " lightyears")
